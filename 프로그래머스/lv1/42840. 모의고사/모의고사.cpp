8cdc8831132c2c34da1baa7fcbdd1afa3caf1298
yuniiya <joypen1267@naver.com>
2023-09-25 12:47:08 +0900

    [level 1] Title: 모의고사, Time: 0.06 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.cpp
@@ -6,47 +6,43 @@ using namespace std;
 
 vector<int> solution(vector<int> answers) {
     vector<int> answer;
-    
-    int A[] = {1, 2, 3, 4, 5};
-    int B[] = {2, 1, 2, 3, 2, 4, 2, 5};
-    int C[] = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};     
-    
+
+    vector<int> A = { 1, 2, 3, 4, 5 };  
+    vector<int> B = { 2, 1, 2, 3, 2, 4, 2, 5 };
+    vector<int> C = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+
     int IdxA = 0;
     int IdxB = 0;
     int IdxC = 0;
-    
-    int Counts[3] {};
-    
-    for(int i = 0; i < answers.size(); i++)
+
+    vector<int> Counts(3);
+
+    for (int i = 0; i < answers.size(); i++)
     {
-        if(A[IdxA] == answers[i])  
-            Counts[0]++;
-        
-        if(B[IdxB] == answers[i])
-            Counts[1]++;
-        
-        if(C[IdxC] == answers[i])
-            Counts[2]++;
-    
-        IdxA = (IdxA + 1) % (sizeof(A) / 4);
-        IdxB = (IdxB + 1) % (sizeof(B) / 4);
-        IdxC = (IdxC + 1) % (sizeof(C) / 4);
+        if (A[IdxA] == answers[i])
+          Counts[0]++;
+
+        if (B[IdxB] == answers[i])
+          Counts[1]++;
+
+        if (C[IdxC] == answers[i])
+          Counts[2]++;
+
+        IdxA = (IdxA + 1) % A.size();
+        IdxB = (IdxB + 1) % B.size();
+        IdxC = (IdxC + 1) % C.size();
     }
-    
-     int Max = 0;
-     for (int i = 0; i < 3; i++)
-     {
-         Max = max(Max, Counts[i]);
-     }
 
-     for (int i = 0; i < 3; i++)
-     {
-         if (Max == Counts[i])
-             answer.push_back(i + 1);
-     }
+    int Max = *max_element(Counts.begin(), Counts.end());
 
-     if (answer.size() > 1)
-         sort(answer.begin(), answer.end());
+    for (int i = 0; i < 3; i++)
+    {
+        if (Max == Counts[i])
+          answer.push_back(i + 1);
+    }
+
+    if (answer.size() > 1)
+        sort(answer.begin(), answer.end());
 
     return answer;
 }
