99dcc086162489a89285ef64a358bdee5479609e
yuniiya <joypen1267@naver.com>
2023-11-27 19:56:26 +0900

    [Bronze II] Title: 농구 경기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/1159. 농구 경기/농구 경기.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int Alpha[26];
+int main()
+{
+	string Ans;
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		string s;
+		cin >> s;
+
+		char a = s[0];
+
+		Alpha[a - 'a']++;
+	}
+
+	for (int i = 0; i < 26; i++)
+	{
+		if (Alpha[i] >= 5)
+			Ans += i + 'a';
+	}
+
+	if (Ans.empty())
+	{
+		cout << "PREDAJA" << "\n";
+	}
+	else
+	{
+		sort(Ans.begin(), Ans.end());
+		cout << Ans << "\n";
+	}
+}
