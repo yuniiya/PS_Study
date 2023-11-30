3cee4e4adc35cf94fee051b4f0133d916bc00bbb
yuniiya <joypen1267@naver.com>
2023-11-30 15:50:12 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,20 +1,17 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
 
 using namespace std;
-
 int N[9];
-vector<int> Ans;
-int sum;
+int Sum;
 pair<int, int> P;
-void solve()
+void Select()
 {
 	for (int i = 0; i < 9; i++)
 	{
 		for (int j = 0; j < i; j++)
 		{
-			if (sum - N[i] - N[j] == 100)
+			if (Sum - N[i] - N[j] == 100)
 			{
 				P = { i, j };
 				return;
@@ -25,31 +22,21 @@ void solve()
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
 
-	// 9명 중에 7명 뽑았을 때, 합이 100
 	for (int i = 0; i < 9; i++)
 	{
-		cin >> N[i];
-		sum += N[i];
+		cin >> N[i]; 
+		Sum += N[i];
 	}
 
-	solve();
+	sort(N, N + 9);
+	Select();
 
-	// 인덱스 i, j는 빼고 출력
 	for (int i = 0; i < 9; i++)
 	{
 		if (i == P.first || i == P.second)
 			continue;
 
-		Ans.push_back(N[i]);
-	}
-
-	sort(Ans.begin(), Ans.end());
-	for (int i : Ans)
-	{
-		cout << i << '\n';
+		cout << N[i] << "\n";
 	}
 }
