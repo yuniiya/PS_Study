1b9c98d86a32a2b167415ebc94690c67f8ac1e4d
yuniiya <joypen1267@naver.com>
2023-12-15 11:11:57 +0900

    [Silver I] Title: 안전 영역, Time: 32 ms, Memory: 2568 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int dy[] = { -1, 0, 1, 0 };
+int dx[] = { 0, -1, 0, 1 };
+
+int h;
+int n;
+int Ans;
+int Ret;
+int tx, ty;
+int N[104][104];
+int M[104][104];
+bool Visited[104][104];
+int Idx[104];
+void dfs(int y, int x)
+{
+	Visited[y][x] = true;
+
+	for (int i = 0; i < 4; i++)
+	{
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+
+		if (ny < 0 || nx < 0 || nx >= n || ny >= n)
+			continue;
+
+		if (Visited[ny][nx] || M[ny][nx] == 0)
+			continue;
+
+		dfs(ny, nx);
+	}
+}
+
+int main()
+{
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> N[i][j];
+			h = max(h, N[i][j]);
+		}
+	}
+
+	int th = h;
+	h--;
+
+	while (h >= 0)
+	{
+		vector<pair<int, int>> V;
+
+		// 영역 계산
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (N[i][j] > h)
+				{
+					M[i][j] = 1;
+					V.push_back({ i, j });
+				}
+			}
+		}
+
+		for (auto i : V)
+		{
+			tie(ty, tx) = i;
+
+			if (!Visited[ty][tx])
+			{
+				dfs(ty, tx);
+				Ret++;
+			}
+		}
+
+		Idx[h] = Ret;
+		Ret = 0;
+
+		fill(&M[0][0], &M[0][0] + 104 * 104, 0);
+		fill(&Visited[0][0], &Visited[0][0] + 104 * 104, false);
+		h--;
+	}
+
+	for (int i = 0; i <= th; i++)
+	{
+		Ans = max(Ans, Idx[i]);
+	}
+
+	cout << Ans << endl;
+}
