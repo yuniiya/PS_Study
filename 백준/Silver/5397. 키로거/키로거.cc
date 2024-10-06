abcd654545e7b3be850db9147288f3c573e42cce
이윤서 <joypen1267@naver.com>
2024-10-06 17:17:41 +0900

    [Silver II] Title: 키로거, Time: 236 ms, Memory: 19384 KB -BaekjoonHub

---
+++ 백준/Silver/5397. 키로거/키로거.cc
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <list>
+
+using namespace std;
+
+int main(void) 
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int a;
+	cin >> a;
+
+	while (a--)
+	{
+		string s;
+		cin >> s;
+
+		list<char> L;
+		list<char>::iterator cursor = L.begin();
+
+		for (char c : s)
+		{
+			if (c == '<')
+			{
+				if (cursor != L.begin())
+				{
+					cursor--;
+				}
+			}
+			else if (c == '>')
+			{
+				if(cursor != L.end())
+				{
+					cursor++;
+				}
+			}
+			else if (c == '-')
+			{
+				if (cursor != L.begin())
+				{
+					cursor--;
+					cursor = L.erase(cursor);
+				}
+			}
+			else
+			{
+				cursor = L.insert(cursor, c);
+
+				if (cursor != L.end())
+				{
+					cursor++;
+				}
+			}
+		}
+
+		for (char c : L)
+		{
+			cout << c;
+		}
+
+		cout << '\n';
+	}
+}
