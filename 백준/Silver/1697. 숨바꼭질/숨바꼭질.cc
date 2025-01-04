9200a87a87a8dbc2eb8bfc85dd334b93a91276af
이윤서 <joypen1267@naver.com>
2025-01-04 16:03:23 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2932 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <tuple>
+
+using namespace std;
+
+int dx[] = { -1, 0, 1, 0 };
+int dy[] = { 0, 1, 0, -1 };
+
+int board[200002];
+int n, k;
+int ans = 987654321;
+queue<int> q;
+
+void Func(int x, int dist)
+{
+	if (x < 0 || x > 100000)
+		return;
+
+	if (board[x] > 0 && x != k)
+		return;
+
+	if (x == k)
+	{
+		ans = min(ans, dist);
+		return;
+	}
+
+	board[x] = dist;
+	q.push(x);
+}
+
+int main() 
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> k;
+
+	if (n == k)
+	{
+		cout << 0;
+		return 0;
+	}
+
+	q.push(n);
+
+	while (!q.empty())
+	{
+		int t = q.front();
+		q.pop();
+
+		Func(t + 1, board[t] + 1);
+		Func(t - 1, board[t] + 1);
+		Func(t * 2, board[t] + 1);
+	}
+
+	cout << ans;
+}
