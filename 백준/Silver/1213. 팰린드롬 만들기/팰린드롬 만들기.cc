983bd9a81526178c680b62a5b0970764b3835199
이윤서 <joypen1267@naver.com>
2025-05-11 21:01:52 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -1,87 +1,63 @@
 #include <iostream>
-#include <string>
 #include <algorithm>
+#include <vector>
+#include <map>
+#include <string>
 
-const int ALPHA_COUNT = 26;
 using namespace std;
+int Alpha[26];
+string Ans;
 int main()
 {
-    int Alpha[ALPHA_COUNT]{};
+	string s;
+	cin >> s;
 
-    string Odd = "";
-    string Even = "";
-    string Result = "";
-    string Name;
+	for (char c : s)
+	{
+		Alpha[c - 'A']++;
+	}
 
-    cin >> Name;
+	// 홀수개 알파벳 개수 체크
+	int OddCnt = 0;
+	int OddIdx = -1;
 
-    for (int i = 0; i < (int)Name.length(); i++)
-    {
-        // 알파벳에 상응하는 인덱스에 개수 저장
-        Alpha[Name[i] - 'A']++;
-    }
+	for (int i = 0; i < 26; i++)
+	{
+		if (Alpha[i] % 2 != 0)
+		{
+			OddCnt++;
+			OddIdx = i;
+		}
+	}
 
-    int OddCount = 0;
-    int Idx = -1;
-    for (int i = 0; i < (int)Name.length(); i++)
-    {
-        // 알파벳 인덱스에 저장된 등장 횟수가 홀수인지 짝수인지
-        if (Alpha[Name[i] - 'A'] % 2 == 0)
-        {
-            Even += Name[i];
-        }
-        else
-        {
-            bool IsFlag = false;
+	if (OddCnt > 1)
+	{
+		cout << "I'm Sorry Hansoo" << endl;
+	}
+	else
+	{
+		if (OddIdx != -1)
+			Alpha[OddIdx]--;
 
-            if (Odd.size() > 0)
-            {
-                for (int j = 0; j < (int)Odd.length(); j++)
-                {
-                    if (Odd[j] == Name[i])
-                    {
-                        IsFlag = true;
-                    }
-                }
-            }
+		for (int i = 0; i < 26; i++)
+		{
+			if (Alpha[i])
+			{
+				for (int j = 0; j < Alpha[i] / 2; j++)
+				{
+					Ans += (char)(i + 'A');
+				}
+			}
+		}
 
-            if (!IsFlag)
-            {
-                Idx = Name[i] - 'A';
-                OddCount++;
-            }
-            Odd += Name[i];
-        }
-    }
+		string Rev = Ans;
+		reverse(Rev.begin(), Rev.end());
 
-    if (OddCount > 1)
-    {
-        cout << "I'm Sorry Hansoo" << endl;
-    }
-    else
-    {
-        for (int i = 0; i < ALPHA_COUNT; i++)
-        {
-            if (Alpha[i] != 0)
-            {
-                // 등장횟수 / 2 번 만큼 더하기
-                for (int j = 0; j < Alpha[i] / 2; j++)
-                {
-                    Result += i + 'A';
-                }
-            }
-        }
-        string Temp = Result;
+		if (OddIdx != -1)
+			Ans += (char)(OddIdx + 'A');
 
-        reverse(Temp.begin(), Temp.end());
+		Ans += Rev;
 
-        // 홀수번 등장한 알파벳이 있다면 
-        if (-1 != Idx)
-        {
-            Result += Idx + 'A';
-        }
-
-        Result += Temp;
-        cout << Result << endl;
-    }
+		cout << Ans << endl;
+	}
 }
