55416da8950e5c39c9d6d86f551d252e9e64ae5d
이윤서 <joypen1267@naver.com>
2025-05-25 21:18:38 +0900

    [Silver I] Title: 쿼드트리, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.cc
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -10,40 +10,36 @@ int dy[] = { -1, 0, 1, 0 };
 int dx[] = { 0, -1, 0, 1 };
 
 int n;
-string s;
+string s, Ans;
 char N[65][65];
 
 string Quard(int y, int x, int Size)
 {
-	// 더이상 쪼갤 수 없다면
 	if (Size == 1)
 		return string(1, N[y][x]);
-
-	string Ret = "";
+	
 	char Target = N[y][x];
+	string Ret = "";
 
 	for (int i = y; i < y + Size; i++)
 	{
 		for (int j = x; j < x + Size; j++)
 		{
-			// 다르다면 크기를 반으로 나눠서 다시 탐색
 			if (Target != N[i][j])
 			{
 				Ret += "(";
+				
+				Ret += Quard(y, x, Size / 2);
+				Ret += Quard(y, x + Size / 2, Size / 2);
+				Ret += Quard(y + Size / 2, x, Size / 2);
+				Ret += Quard(y + Size / 2, x + Size / 2, Size / 2);
 
-				Ret += Quard(y, x, Size / 2);							// 왼쪽 위
-				Ret += Quard(y, x + Size / 2, Size / 2);				// 오른쪽 위
-				Ret += Quard(y + Size / 2, x, Size / 2);				// 왼쪽 아래
-				Ret += Quard(y + Size / 2, x + Size / 2, Size / 2);		// 오른쪽 아래
-
-				// 네 부분 모두 탐색을 마쳤다면 닫아서 반환
 				Ret += ")";
 				return Ret;
 			}
 		}
 	}
 
-	// 다르지 않다면
 	return string(1, Target);
 }
 
@@ -61,5 +57,5 @@ int main()
 		}
 	}
 
-	cout << Quard(0, 0, n) << endl;
+	cout << Quard(0, 0, n);
 }
