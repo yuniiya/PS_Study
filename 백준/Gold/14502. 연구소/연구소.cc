7d0184bb37111c427e3176cedaeb5fcd31d491ba
이윤서 <joypen1267@naver.com>
2025-06-08 21:19:46 +0900

    [Gold IV] Title: 연구소, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -1,56 +1,67 @@
 #include <iostream>
 #include <algorithm>
+#include <queue>
+#include <tuple>
 #include <vector>
+#include <map>
+#include <string>
+#include <iomanip>
+#include <stack>
+#include <memory.h>
 
 using namespace std;
 
-int n, m;
-int N[9][9];
-bool Visited[9][9];
-int Ans;
-
 const int dy[] = { -1, 0, 1, 0 };
-const int dx[] = { 0, 1, 0, -1 };
+const int dx[] = { 0, 1, 0,-1 };
 
-vector<pair<int, int>> ZeroList;
-vector<pair<int, int>> VirusList;
+int n, m;
+int ans = -1;
+int N[9][9];
+int visited[9][9];
+vector<pair<int, int>> zeros;
+vector<pair<int, int>> virus;
 
 void dfs(int y, int x)
 {
-	Visited[y][x] = true;
+	if (visited[y][x])
+		return;
+
+	visited[y][x] = 1;
 
 	for (int i = 0; i < 4; i++)
 	{
 		int ny = dy[i] + y;
 		int nx = dx[i] + x;
 
-		if (ny < 0 || ny >= n || nx < 0 || nx >= m || Visited[ny][nx] || N[ny][nx] == 1)
+		if (ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx] || N[ny][nx] == 1)
 			continue;
 
 		dfs(ny, nx);
 	}
 }
 
-int solve()
+void goVirus()
 {
-	// 조합의 경우의 수마다 Visited 초기화
-	fill(&Visited[0][0], &Visited[0][0] + 9 * 9, false);
+	memset(visited, 0, sizeof(visited));
 
-	// 바이러스 퍼트리기 -> 바이러스가 있는 위치는 Visited로 체크
-	for (int i = 0; i < VirusList.size(); i++)
+	for (pair<int, int> p : virus)
 	{
-		dfs(VirusList[i].first, VirusList[i].second);
+		dfs(p.first, p.second);
 	}
-	
-	// 0의 개수 세기
+}
+
+int go()
+{
 	int Ret = 0;
+
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < m; j++)
 		{
-			// 0이었으면서 바이러스가 새롭게 퍼지지 않은 위치라면 
-			if (N[i][j] == 0 && !Visited[i][j])
+			if (!visited[i][j] && N[i][j] == 0)
+			{
 				Ret++;
+			}
 		}
 	}
 
@@ -68,32 +79,38 @@ int main()
 			cin >> N[i][j];
 
 			if (N[i][j] == 0)
-				ZeroList.push_back({ i, j });
-			else if (N[i][j] == 2)
-				VirusList.push_back({ i, j });
+				zeros.push_back({ i, j });
+			if (N[i][j] == 2)
+				virus.push_back({ i, j });
 		}
 	}
 
-	for (int i = 0; i < ZeroList.size(); i++)
+	for (int i = 0; i < zeros.size(); i++)
 	{
 		for (int j = 0; j < i; j++)
 		{
 			for (int k = 0; k < j; k++)
 			{
 				// 벽 세우기
-				N[ZeroList[i].first][ZeroList[i].second] = 1;
-				N[ZeroList[j].first][ZeroList[j].second] = 1;
-				N[ZeroList[k].first][ZeroList[k].second] = 1;
+				N[zeros[i].first][zeros[i].second] = 1;
+				N[zeros[j].first][zeros[j].second] = 1;
+				N[zeros[k].first][zeros[k].second] = 1;
 
-				Ans = max(Ans, solve());
+				// 바이러스 퍼트리기
+				goVirus();
 
-				// 원복
-				N[ZeroList[i].first][ZeroList[i].second] = 0;
-				N[ZeroList[j].first][ZeroList[j].second] = 0;
-				N[ZeroList[k].first][ZeroList[k].second] = 0;
+				// 영역 넓이 계산
+				ans = max(ans, go());
+
+				// 벽 원복
+				N[zeros[i].first][zeros[i].second] = 0;
+				N[zeros[j].first][zeros[j].second] = 0;
+				N[zeros[k].first][zeros[k].second] = 0;
 			}
 		}
 	}
 
-	cout << Ans << "\n";
+	cout << ans << "\n";
 }
+
+
