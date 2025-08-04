05dbcb0a25bce1264e0a71196841944089e4d764
이윤서 <joypen1267@naver.com>
2025-08-04 23:58:32 +0900

    [Gold IV] Title: 오큰수, Time: 204 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -1,33 +1,60 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
 #include <stack>
+#include <queue>
+#include <tuple>
 #include <memory.h>
+#include <map>
+#include <unordered_map>
 
 using namespace std;
 
-int n;
-int N[1000001], Ans[1000001];
+int N[1000002][2];
+int n, a;
 stack<int> st;
 int main()
 {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	memset(N, -1, sizeof(N));
 	cin >> n;
 
-	memset(Ans, -1, sizeof(Ans));
-
 	for (int i = 0; i < n; i++)
 	{
-		cin >> N[i];
-
-		while (st.size() && N[st.top()] < N[i])
+		cin >> a;
+		N[i][0] = a;
+		
+		while (st.size() > 0 && a > N[st.top()][0])
 		{
-			Ans[st.top()] = N[i];
-			st.pop();
+			int t = st.top();
+			if (a > N[t][0])
+			{
+				N[t][1] = a;
+				st.pop();
+			}
 		}
 
-		st.push(i);
+		if (i > 0)
+		{
+			if (N[i - 1][1] == -1 && a > N[i - 1][0])
+			{
+				N[i - 1][1] = a;
+			}
+			else
+			{
+				st.push(i - 1);
+			}
+		}
 	}
 
 	for (int i = 0; i < n; i++)
 	{
-		cout << Ans[i] << " ";
+		cout << N[i][1] << " ";
 	}
+
+	return 0;
 }
